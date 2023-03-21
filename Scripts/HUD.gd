ab862c58d51b072eb23c24bb2072d6904b894ba1
kikinizzy03 <01134.world@gmail.com>
2023-03-21 09:41:55 -0400

    Added game files to repo

---
+++ Scripts/HUD.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var curr_time = 30
+
+func _ready():
+	$Message.hide()
+	$Countdown.start()
+	$Time.text = str(curr_time)
+	
+	$Time.show()
+
+
+func _on_Countdown_timeout():
+	curr_time -= 1
+	$Time.text = str(curr_time)
+	
+	if(curr_time <= 0):
+		$Countdown.stop()
+		$Message.show()
+		$Message.text = "Game Over"
