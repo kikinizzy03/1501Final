4eb31dabee763968733365575ab5b96cd7827932
kikinizzy03 <01134.world@gmail.com>
2023-03-23 12:10:30 -0400

    Added functionality to GameOver scene

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -4,7 +4,6 @@ export (String, FILE, "*.tscn") var target_level = ""
 export var curr_time = 30
 
 func _ready():
-	$Message.hide()
 	$Countdown.start()
 	$Time.text = str(curr_time)
 	
@@ -17,8 +16,6 @@ func _on_Countdown_timeout():
 	
 	if(curr_time <= 0):
 		$Countdown.stop()
-		$Message.show()
-		$Message.text = "Game Over"
 		
 		$Music.stop()
 		get_tree().change_scene(target_level)
